3e5baf07c828d6c242723b8e6456dfb8ef1b56c1
NEYKER <julianm2023@hotmail.com>
2021-10-16 17:01:15 -0500

    Rotacion completada
    la camara ya realiza la rotacion y acercamiento al objeto aunque no pude realizarlo con el RotateAround debido a que generaba un error de inestabilidad osea la camara giraba en direcciones erroneas asi que lo cambie con un Rotate comun el cual junto a su variable angle multiplicado por un valor angular en esta caso 180 "grados" ya no presento este problema

--- Assets/Scripts/CameraLookController.cs
+++ Assets/Scripts/CameraLookController.cs
@@ -3,16 +3,56 @@ using UnityEngine;
 public class CameraLookController : MonoBehaviour
 {
     Camera cam = null;
+    Transform target = null;
 
-    // Start is called before the first frame update
-    void Start()
+    float zoom = 1; //zoom que es asignado junto con la rueda del raton para acercarse o alejarse del objeto
+
+    float zoomAmount = 50;// multiplicador de velocidad 
+
+    Vector3 previousPosition = new Vector3(0,0,0);
+
+    void Start()//encuentra las referencias de los objetos
     {
-        
+        cam = FindObjectOfType<Camera>();
+        target = GameObject.Find("Ditto").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RotateAroundObject();
+    }
+
+    public void RotateAroundObject() //Controla la rotacion y limites de acercamiento de la camara
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = previousPosition - cam.ScreenToViewportPoint(Input.mousePosition);
+
+            cam.transform.position = target.position;
+
+            cam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+            cam.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);
+
+            cam.transform.Translate(new Vector3(0, 0, -zoom));
+
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            zoom -= zoomAmount * Time.deltaTime;
+        }
+
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            zoom += zoomAmount * Time.deltaTime;
+        }
+
+        zoom = Mathf.Clamp(zoom, 0.8f, 20f);
     }
 }
