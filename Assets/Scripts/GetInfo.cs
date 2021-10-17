bb0a3afbe3492afe3b8760c210c1551850b83329
NEYKER <julianm2023@hotmail.com>
2021-10-17 13:42:49 -0500

    Avance cargado de datos
    vale ya pude realizar el cargado de datos y bueno aun falta cargar la imagen de referncia del personaje y arreglar la funcionalidad del scroll bar que se adapte a el tamanio del canvas aun asi no voy a mostrar todos los datos del archivo debido a que son demasiado excesivos y bueno a largo plazo no habria problema de colocarlos pero no creo que sean necesarios de momento por ende solo coloque datos relevantes para una wiki de un pokemon los siguientes pasos seran empezar a trabajar en el raycast para instancias la pelota que realizara la colicion y bueno todo lo que eso implica pero antes terminare de arrelgar los detalles faltantes

--- Assets/Scripts/GetInfo.cs
+++ Assets/Scripts/GetInfo.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 using UnityEngine.Networking;
 using TMPro;
 using SimpleJSON;
-
 public class GetInfo : MonoBehaviour
 {
+    [SerializeField] string urlPokemon = null;
     [SerializeField] TextMeshProUGUI textNamePokemon = null;
 
-    public void Start() => StartCoroutine(GetApiDataCoroutine());
+    [SerializeField] GameObject textPrefab = null;
+    GameObject textinstanced = null;
+    [SerializeField] RectTransform content = null;
 
+    [SerializeField] Transform parentInstance = null;
+
+    Quaternion initialRotation = new Quaternion(0, 0, 0, 0);
+
+    public void Start() => StartCoroutine(GetApiDataCoroutine());
     IEnumerator GetApiDataCoroutine() 
     {
         textNamePokemon.text = "Loading...";
-        string url = "https://pokeapi.co/api/v2/pokemon/ditto";
-        UnityWebRequest request = UnityWebRequest.Get(url);
+
+        UnityWebRequest request = UnityWebRequest.Get(urlPokemon);
 
         yield return request.SendWebRequest();
 
@@ -25,8 +32,95 @@ public class GetInfo : MonoBehaviour
             yield break;
         }
 
+        //Name
         JSONNode dataRecive = JSON.Parse(request.downloadHandler.text);
 
         textNamePokemon.text = dataRecive["name"];
+
+        //ID
+
+        textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+        textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "ID:" + dataRecive["id"];
+
+        //Base experience
+
+        textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+        textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Base experience:" + dataRecive["base_experience"];
+
+        //Order
+
+        textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+        textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Order:" + dataRecive["order"];
+
+        //Height
+
+        textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+        textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Height:" + dataRecive["height"];
+
+        //Weight
+
+        textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+        textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Weight:" + dataRecive["weight"];
+
+        //species
+
+        textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+        textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Species:" + dataRecive["species"]["name"];
+
+
+        //abilities
+        JSONNode pokeAbilities = dataRecive["abilities"];
+        
+        for (int i = 0; i < pokeAbilities.Count; i++)
+        {
+            textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+            textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Ability:" +  pokeAbilities[i]["ability"]["name"];
+        }
+
+        //Types
+        JSONNode pokeTypes = dataRecive["types"];
+
+        for (int i = 0; i < pokeTypes.Count; i++)
+        {
+            textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation,parentInstance);
+
+            textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Type:" + pokeTypes[i]["type"]["name"];
+        }
+
+        //Moves
+        JSONNode pokeMoves = dataRecive["moves"];
+
+        for (int i = 0; i < pokeMoves.Count; i++)
+        {
+            textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+            textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Move:" + pokeMoves[i]["move"]["name"];
+        }
+
+        //held items
+        JSONNode pokeHeldItems = dataRecive["held_items"];
+
+        for (int i = 0; i < pokeHeldItems.Count; i++)
+        {
+            textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+            textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Held Items:" + pokeHeldItems[i]["item"]["name"];
+        }
+
+        JSONNode pokeStats = dataRecive["stats"];
+
+        for (int i = 0; i < pokeStats.Count; i++)
+        {
+            textinstanced = Instantiate(textPrefab, parentInstance.transform.position, initialRotation, parentInstance);
+
+            textinstanced.GetComponentInChildren<TextMeshProUGUI>().text = "Stat-" + pokeStats[i]["stat"]["name"] + ":" + pokeStats[i]["base_stat"];
+        }
     }
 }
